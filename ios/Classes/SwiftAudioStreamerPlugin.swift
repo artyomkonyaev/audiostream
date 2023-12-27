8d5fc28e647aea222818813b93f5aa42580cc78b
Artyom <konyaevartyom@gmail.com>
2023-12-27 07:49:06 +0300

    testing influence on audioplayback

--- ios/Classes/SwiftAudioStreamerPlugin.swift
+++ ios/Classes/SwiftAudioStreamerPlugin.swift
@@ -126,7 +126,7 @@ public class SwiftAudioStreamerPlugin: NSObject, FlutterPlugin, FlutterStreamHan
 
     do {
       try AVAudioSession.sharedInstance().setCategory(
-        AVAudioSession.Category.playAndRecord, options: .mixWithOthers)
+        AVAudioSession.Category.playAndRecord)
       try AVAudioSession.sharedInstance().setActive(true)
 
       if let sampleRateNotNull = sampleRate {
